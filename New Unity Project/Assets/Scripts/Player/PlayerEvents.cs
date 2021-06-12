81bc2e016c0d58b3cd8dab341126154d2f0f5c79
joseph-chew <56200324+joseph-chew@users.noreply.github.com>
2021-06-11 22:58:30 -0400

    - Add the Player and gameplay scripts to get us started - modifies the `.gitignore` so it exist in the correct place and ignores the correct directories - adds the Library, Temp, and Logs directories to the `.gitignore` which looks like a "delete" operation to git and the commit

---
+++ New Unity Project/Assets/Scripts/Player/PlayerEvents.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace PlayerEvents
+{
+   public struct ShapeShift : iEvent
+   {
+        public readonly PState setState;
+
+        public ShapeShift(PState stateToSet)
+        {
+            setState = stateToSet;
+        }
+   }
+
+   public struct ObsCollision : iEvent
+   {
+       public readonly GameObject obstacle;
+
+       public readonly Action<bool> isCorrectState;
+
+       public ObsCollision(GameObject _obstacle, Action<bool> stateCheckCallback )
+       {
+           obstacle = _obstacle;
+           isCorrectState = stateCheckCallback;
+       }
+   }
+
+    public struct SendTransform : iEvent
+    {
+        public readonly Transform playerTransform;
+
+        public SendTransform(Transform pTransform)
+        {
+            playerTransform = pTransform;
+        }
+    }
+
+    public struct RespawnPlayer : iEvent 
+    {
+        public Transform playerTransform;
+
+        public RespawnPlayer(Transform referenceToPlayerTransform)
+        {
+            playerTransform = referenceToPlayerTransform;
+        }
+    }
+}
