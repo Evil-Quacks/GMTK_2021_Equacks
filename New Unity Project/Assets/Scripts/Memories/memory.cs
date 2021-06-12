0ad533aa81bd9bef111964cff872f5983a7b4df4
Amanda Barbadora <a.barbadora@gmail.com>
2021-06-12 01:51:58 -0400

    Base Structre for memories.

---
+++ New Unity Project/Assets/Scripts/Memories/memory.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class memory
+{
+    public AudioSource memorySFX;
+    public bool correct;
+    public int attempts;
+}
